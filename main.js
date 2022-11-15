58e4da109e6969ea3990feec498fabc8a09b96af
rachitkhope <106763102+rachitkhope@users.noreply.github.com>
2022-11-15 20:14:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getcontext("2d")
+
+ctx.beginpath ();
+ctx.storkestyle ="grey";
+ctx.lineWidth = 1;
+ctx.rect(150, 143 ,430 ,200,);
+ctx.storke();
+
+
+
+ctx.beginpath ();
+ctx.storkestyle ="blue";
+ctx.lineWidth = 5;
+ctx.arc( 250, 210 ,40 ,0 ,2 * Math.PI);
+ctx.storke();
+
+
+ctx.beginpath ();
+ctx.storkestyle ="black";
+ctx.lineWidth = 5;
+ctx.arc(350, 210 ,40 ,0,2 * Math.PI);
+ctx.storke();
+
+
+ctx.beginpath ();
+ctx.storkestyle ="red";
+ctx.lineWidth = 5;
+ctx.arc(450, 210 ,40 ,0,2 * Math.PI);
+ctx.storke();
+
+
+ctx.beginpath ();
+ctx.storkestyle ="yellow";
+ctx.lineWidth = 5;
+ctx.arc(300, 250 ,40 ,0,2 * Math.PI);
+ctx.storke();
+
+
+ctx.beginpath ();
+ctx.storkestyle ="green";
+ctx.lineWidth = 5;
+ctx.arc(250, 210 ,40 ,0,2 * Math.PI);
+ctx.storke();
